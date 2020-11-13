86ca889ac91e98bf6be72d8d0e79d1cca4e38b09
goodingc <callum.gooding11@gmail.com>
2020-11-13 14:07:45 +0000

    Update followbot_frame.py

--- followbot_frame.py
+++ followbot_frame.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
-import cv2, math
-from random import random
+import cv2
+import math
 from math import radians, atan2
+from random import random
+
 import cv_bridge
 import numpy as np
 import rospy
 import tf
-from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import Twist, Vector3
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, CameraInfo
+from sensor_msgs.msg import LaserScan
 
 
 class Follower:
@@ -29,6 +32,7 @@ class Follower:
         self.bridge = cv_bridge.CvBridge()
         rospy.Subscriber('camera/rgb/image_raw', Image, self.handle_image)
         rospy.Subscriber('camera/rgb/camera_info', CameraInfo, self.handle_camera_info)
+        rospy.Subscriber('scan', LaserScan, handle_scan)
         rospy.Subscriber('odom', Odometry, self.handle_odom)
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
@@ -43,12 +47,15 @@ class Follower:
         @return: A boolean image where input pixels that are green are marked True in the output image and all others
         are marked False.
         """
-        
-        HSV_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # Convert RGB image to HSV colour space
-        Low_HSV = np.array([50,0,0],dtype='uint8') # Bottom end of green hue range
-        High_HSV = np.array([70,255,255],dtype='uint8') # Top end of green hue range
-        
-        Thresholded_Image = cv2.inRange(HSV_image, Low_HSV, High_HSV) # Apply thresholding to convert to boolean image (white pixels = green object, black = background)
+
+        HSV_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)  # Convert RGB image to HSV colour space
+        Low_HSV = np.array([50, 0, 0], dtype='uint8')  # Bottom end of green hue range
+        High_HSV = np.array([70, 255, 255], dtype='uint8')  # Top end of green hue range
+
+        Thresholded_Image = cv2.inRange(HSV_image, Low_HSV,
+                                        High_HSV)
+        # Apply thresholding to convert to boolean image (white pixels =
+        # green object, black = background)
 
         return Thresholded_Image
 
@@ -67,7 +74,6 @@ class Follower:
         diff_y = beacon_coords[1] - self.y
         return atan2(diff_y, diff_x)
 
-
     def move_robot(self, angle_offset):
         """
         Moves the robot forwards in the direction of the beacon using the offset angle.
@@ -81,19 +87,19 @@ class Follower:
         The value of self.wandering should be False for the first time this is called after having performed homing
         behaviour and then True for all times after that.
         """
-        #declaring linear velocity
+        # declaring linear velocity
         forward_vel = Twist()
         forward_vel.linear.x = 0.4
-        #declaring angular velocity
+        # declaring angular velocity
         angular_vel = Twist()
         angular_vel.linear.x = 0
         angular_vel.angular.z = radians(45)
         rand_direc = random.randint(0, 80)
-        for n in range(0, 75):#moves forward 3 meters in 7.5 seconds
+        for n in range(0, 75):  # moves forward 3 meters in 7.5 seconds
             self.pub.publish(forward_vel)
             self.avoid()
             self.rate_limiter.sleep()
-        #turns for a random time between(0, 80) which is ewuivelat to turning between(0 dgrees - 360 degrees)
+        # turns for a random time between(0, 80) which is ewuivelat to turning between(0 dgrees - 360 degrees)
         for n in range(rand_direc):
             self.pub.publish(angular_vel)
             self.rate_limiter.sleep()
@@ -120,8 +126,8 @@ class Follower:
         Sets self.image with the value of the BGR encoded image from msg
         @param msg: The message received on the camera/rgb/image_raw topic
         """
-        self.image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-        cv2.imshow("window",self.image)
+        self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        cv2.imshow("window", self.image)
         cv2.waitKey(3)
 
     def handle_camera_info(self, msg):
@@ -138,14 +144,15 @@ class Follower:
         Sets self.[theta, x, y] as their respective values parsed from msg
         @param msg: The message received on the odom topic
         """
-         quarternion = [msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,\
-                    msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
-        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(quarternion)
+        quarternion = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, \
+                       msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
 
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(quarternion)
+    
         self.x = msg.pose.pose.position.x
         self.y = msg.pose.pose.position.y
         self.theta = yaw
-    
+
     def handle_sensor(self, msg):
         self.f = msg.ranges[0]
         self.r = msg.ranges[250]
@@ -161,7 +168,7 @@ class Follower:
         angular_vel_l.angular.z = radians(-90)
         b = self.r > self.l
         while self.f < self.front_range:
-            ang_vel = (angular_vel_l if b==True else angular_vel_r)
+            ang_vel = (angular_vel_l if b is True else angular_vel_r)
             self.pub.publish(ang_vel)
             self.rate_limiter.sleep()
 
